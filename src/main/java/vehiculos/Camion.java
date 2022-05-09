a153d475227d559b2ea99149b2caf2dbd3a478f7
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-05-09 18:26:24 -0500

    Update Camion.java

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -5,8 +5,8 @@ public class Camion extends Vehiculo{
 	private int ejes;
 	private static int camiones = 0;
 
-	public Camion(String placa, int puertas, float velocidadMaxima, String nombre, float precio, float peso,
-			String traccion, Fabricante fabricante, int ejes) {
+	public Camion(String placa, String nombre, int precio, float peso,
+			Fabricante fabricante, int ejes) {
 		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
 		this.ejes = ejes;
 		camiones++;
