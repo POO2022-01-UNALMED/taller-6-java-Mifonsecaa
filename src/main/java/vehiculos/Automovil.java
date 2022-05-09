7cef9eec08e79538951cbab7e0555f0f665d47c3
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-05-09 18:01:47 -0500

    Update Automovil.java

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -3,21 +3,20 @@ package vehiculos;
 public class Automovil extends Vehiculo{
 	
 	private int puertas;
-	private static int CantidadAutomovil;
+	private static int automoviles = 0;
 
 	public Automovil(String placa, int puertas, float velocidadMaxima, String nombre, float precio, float peso,
 			String traccion, Fabricante fabricante) {
 		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
 		
 		this.puertas = puertas;
-		Automovil.CantidadAutomovil = Automovil.CantidadAutomovil + 1;
+		automoviles++;
 		
 	}
 	
 	public int getPuertas() {return puertas;}
-	public void setPuertas(int x) {this.puertas = x;}
+	public void setPuertas(int puertas) {this.puertas = puertas;}
 	
-	public static int getCantidadAutomovil(){return Automovil.CantidadAutomovil;}
-	public static void setCantidadAutomovil(int x) {Automovil.CantidadAutomovil = x;}
+	public static int getCantidadAutomovil(){return automoviles;}
 	
 }
