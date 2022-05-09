1f0d05cfffefbae4716dedc90a7acc0bdd56d27a
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-05-09 18:09:46 -0500

    Update Camioneta.java

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -6,7 +6,7 @@ public class Camioneta extends Vehiculo{
 	private static int camionetas = 0;
 	
 	public Camioneta (String placa, int puertas, float velocidadMaxima, String nombre, float precio, float peso,
-			String traccion, Fabricante fabricante) {
+			String traccion, Fabricante fabricante, boolean volco) {
 		super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
 		this.volco = volco;
 		camionetas++;
